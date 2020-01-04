dd2a83ba91157bb87c3bdee451506b3a07d15cad
Michael Dunsterville <michael@klientboost.com>
2020-01-04 09:50:50 -0800

    Setup Heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 require('./config')
 const express = require('express')
 const { join } = require('path')
 const app = express()
+
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
