96b3a500beabdadaef85e366b5ddce552e7ad7d4
Bert Keller <bertwinkeller@gmail.com>
2020-01-11 09:55:15 -0800

    article links

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -3,7 +3,7 @@ const { Note } = require('../models')
 module.exports = app => {
 
   // Get all notes
-  app.get('/notes', (req,res) => {
+  app.get('/notes', (req, res) => {
     Note.find()
       .populate('articles')
       .then(notes => res.json(notes))
@@ -11,9 +11,21 @@ module.exports = app => {
   })
 
   // Post on notes
-  app.post('./notes', (req,res) => {
+  app.post('/notes', (req, res) => {
     Note.create(req.body)
       .then(() => res.sendStatus(200))
       .catch(err => console.error(err))
+  })
+
+  app.delete('/notes/:id', (req, res) => {
+    Note.findOneAndDelete({ __id: parseInt(req.params.id) }, function (err) {
+      if (err) {
+        console.log(err)
+        return res.sendStatus(500).send()
+      }
+      return res.sendStatus(200).send()
+    }
+    )
+
   })
 }
